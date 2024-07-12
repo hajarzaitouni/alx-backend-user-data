0f63da9bb7809f4818e6f2f10c2a8f1f102c7099
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-12 16:12:06 +0100

    update auth file

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,18 @@ class Auth():
     """ create a class Auth """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Defines if the path requires authentication """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if not path.endswith('/'):
+            path += '/'
+
+        for ex_path in excluded_paths:
+            if path == ex_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Returns the authorization header """
