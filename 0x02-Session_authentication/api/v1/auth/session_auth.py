32da2dc058b17939425e3e78e63ac8cf650084e7
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-13 13:37:53 +0100

    create empty session, task 1

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Define SessionAuth module """
+
+
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """ creating session authentication class """
+    pass
