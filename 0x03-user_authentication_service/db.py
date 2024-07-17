f50921bfbb53d383250ff7bb59d2d7f90f4f649e
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-17 18:58:37 +0100

    update db file by adding a filter method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         self._session.commit()
         return user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """ Find a user """
         if not kwargs:
             raise InvalidRequestError
