16172e288a82d02e4097ee1019dc492bb1b446b5
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-07-17 21:20:07 +0100

    update app by adding a new end point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
 """ Set up a basic Flask app """
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello():
-    """ return a jsonify message """
+    """ return a hello message """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email,
+                        "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
